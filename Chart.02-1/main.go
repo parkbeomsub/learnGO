0ddb60936eec6f4dd112f8f2b433d2ea17ba7034
ParkDT <okas123852@gmail.com>
2024-08-05 16:38:27 +0900

    ch2

--- Chart.02-1/main.go
+++ Chart.02-1/main.go
@@ -7,17 +7,36 @@ import (
 
 func main() {
 
-	dictionary := mydict.Dictionary{"frist": "first word"}
-	fmt.Println(dictionary["frist"])
+	// dictionary := mydict.Dictionary{"frist": "first word"}
+	// fmt.Println(dictionary["frist"])
 
-	dictionary["hello"] = "hello"
-	fmt.Println(dictionary)
-	fmt.Println("----")
+	// dictionary["hello"] = "hello"
+	// fmt.Println(dictionary)
+	// fmt.Println("----")
 
-	definiton, err := dictionary.Search("Second")
+	// definiton, err := dictionary.Search("Second")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(definiton)
+	// }
+
+	// err = dictionary.Add("hello1", "hello2")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// hello, err := dictionary.Search("hello2")
+
+	// fmt.Println(hello)
+
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	dictionary.Add(word, "bye")
+	err := dictionary.Update(word, "what's up?")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definiton)
 	}
+	word, _ = dictionary.Search((word))
+	fmt.Println(word)
+
 }
--- Chart.02-1/mydict/mydict.go
+++ Chart.02-1/mydict/mydict.go
@@ -5,7 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not found")
+var (
+	errNotFound   = errors.New("Not found")
+	errWordExists = errors.New("Word Exists")
+	errCantUpdate = errors.New("That word already exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -15,4 +19,37 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definiton string) error {
+
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definiton
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+
 }
